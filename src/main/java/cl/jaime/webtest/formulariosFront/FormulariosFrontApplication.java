a69d7796fa54cd6a8436e570cfe826c8090d0396
jai-pavez <jai.pavez@gmail.com>
2021-10-30 01:00:25 -0300

    mod

--- src/main/java/cl/jaime/webtest/formulariosFront/FormulariosFrontApplication.java
+++ src/main/java/cl/jaime/webtest/formulariosFront/FormulariosFrontApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cl.jaime.webtest.formulariosFront")
 public class FormulariosFrontApplication extends SpringBootServletInitializer {
 
 	@Override 
--- src/main/java/cl/jaime/webtest/formulariosFront/controller/MainPageController.java
+++ src/main/java/cl/jaime/webtest/formulariosFront/controller/MainPageController.java
@@ -2,16 +2,20 @@ package cl.jaime.webtest.formulariosFront.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainPageController {
 	
-	@GetMapping("/index")
-	public String index(Model model) {
-		
-		return "index";
+	@GetMapping("/index") 
+	public String   index(Model model) {
+		System.out.println(" estoy en el index saludos");
+        
+
+		return "index" ;
 	}
 }
